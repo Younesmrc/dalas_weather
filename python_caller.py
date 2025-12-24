c3bff57794316b2a7dc4c938ed70758394055821
HaidarFico <haidarficoi@gmail.com>
2025-12-24 19:28:38 +0100

    working version

--- python_caller.py
+++ python_caller.py
@@ -3,26 +3,27 @@ import csv
 import threading
 import os.path
 from datetime import datetime
+from zoneinfo import ZoneInfo
 import web_scrapper_selenium
 # accidents_2022_2023_meteo.csv
 
 def run(lowerBound: int, upperBound: int, csvFileName: str):
-    with open(csvFileName, 'r', newline='') as csvFile:
-        reader = csv.DictReader(csvFile)
+    with open(csvFileName, 'r') as csvFile:
+        reader = csv.DictReader(csvFile, delimiter=';')
         rows = list(reader)
+
+        # threads = []
+        # results = []
 
         for i in range(lowerBound, min(upperBound + 1, len(rows))):
             row = rows[i]
             print(row)
             lat = row["lat"]
             lon = row["long"]
-            # time = row["time"]
-            # day =
-            # month
-            # year
-            # hour
-            fullDate = datetime.strptime(row['date'], '%Y-%m-%d')
-            fullDateFormated = fullDate.isoformat()
+            hour = row['hrmn']
+            date = f'{row['an']}-{row['mois']}-{row['jour']}'
+            fullDate = f'{date} {hour}'
+            fullDateFormated = datetime.strptime(fullDate, '%Y-%m-%d %H:%M').replace(tzinfo=ZoneInfo("Europe/Paris")).isoformat()
             id = row['Num_Acc']
 
             web_scrapper_selenium.run(lat, lon, fullDateFormated, id, f'{lowerBound}_{upperBound}')
@@ -33,8 +34,5 @@ if __name__ == '__main__':
     lowerBound = int(sys.argv[1])
     upperBound = int(sys.argv[2])
     fileName = sys.argv[3]
-    print(lowerBound)
-    print(upperBound)
-    print(fileName)
     run(lowerBound, upperBound, fileName)
     # csvFile = openCSVFile(fileName)
--- web_scrapper_selenium.py
+++ web_scrapper_selenium.py
@@ -10,10 +10,10 @@ import os.path
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
 import re
 
-API_KEY=''
+API_KEY='bdc_3f00dbcc437e48e2be3dd32756eb5577'
 
 def to_float(text: str) -> float:
     match = re.search(r"-?\d+(?:\.\d+)?", text)
@@ -25,66 +25,82 @@ def getScrappedData(cityName, timeOfDay: datetime.datetime):
     url = f"https://www.wunderground.com/history/daily/fr/{cityName}/date/{timeOfDay.year}-{timeOfDay.month}-{timeOfDay.day}"
     hour = timeOfDay.hour
 
-    driver = uc.Chrome()
-    wait = WebDriverWait(driver, 30)
-    print(url)
-    driver.get(url)
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options )
+    wait = WebDriverWait(driver, 10)
+    try:
+        print(url)
+        driver.get(url)
 
-    # Wait until hourly rows exist in live DOM
-    rows = wait.until(
-        EC.presence_of_all_elements_located(
-            (By.CSS_SELECTOR, ".observation-table tbody tr")
+        # Wait until hourly rows exist in live DOM
+        rows = wait.until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, ".observation-table tbody tr")
+            )
         )
-    )
 
-    row = rows[hour]
+        row = rows[hour]
 
-    cells = row.find_elements(By.TAG_NAME, "td")
+        cells = row.find_elements(By.TAG_NAME, "td")
 
-    # second_rows = wait.until(
-    #     EC.presence_of_all_elements_located(
-    #         (By.CSS_SELECTOR, ".summary-table tbody tr")
-    #     )
-    # )
+        # second_rows = wait.until(
+        #     EC.presence_of_all_elements_located(
+        #         (By.CSS_SELECTOR, ".summary-table tbody tr")
+        #     )
+        # )
 
-    temperature = cells[1].get_attribute("textContent").strip()
-    dew_point = cells[2].get_attribute("textContent").strip()
-    humidity = cells[3].get_attribute("textContent").strip()
-    wind = cells[4].get_attribute("textContent").strip()
-    wind_speed = cells[5].get_attribute("textContent").strip()
-    wind_gust = cells[6].get_attribute("textContent").strip()
-    pressure = cells[7].get_attribute("textContent").strip()
-    precipitation = cells[8].get_attribute("textContent").strip()
-    condition = cells[9].get_attribute("textContent").strip()
-    # visibility = re.findall(
-    #     r"\d+", second_rows[9].get_attribute("textContent").strip()
-    # )[0]
+        temperature = cells[1].get_attribute("textContent").strip()
+        dew_point = cells[2].get_attribute("textContent").strip()
+        humidity = cells[3].get_attribute("textContent").strip()
+        wind = cells[4].get_attribute("textContent").strip()
+        wind_speed = cells[5].get_attribute("textContent").strip()
+        wind_gust = cells[6].get_attribute("textContent").strip()
+        pressure = cells[7].get_attribute("textContent").strip()
+        precipitation = cells[8].get_attribute("textContent").strip()
+        condition = cells[9].get_attribute("textContent").strip()
+        # visibility = re.findall(
+        #     r"\d+", second_rows[9].get_attribute("textContent").strip()
+        # )[0]
 
-    second_rows = driver.find_elements(By.CSS_SELECTOR, ".summary-table tbody tr")
+        second_rows = driver.find_elements(By.CSS_SELECTOR, ".summary-table tbody tr")
 
-    if second_rows:
-        # safely extract visibility
-        visibility_text = second_rows[9].get_attribute("textContent").strip() if len(second_rows) > 9 else None
-        numbers = re.findall(r"\d+", visibility_text) if visibility_text else []
-        visibility = numbers[0] if numbers else None
-    else:
-        visibility = None
+        if second_rows:
+            # safely extract visibility
+            visibility_text = second_rows[9].get_attribute("textContent").strip() if len(second_rows) > 9 else None
+            numbers = re.findall(r"\d+", visibility_text) if visibility_text else []
+            visibility = numbers[0] if numbers else None
+        else:
+            visibility = None
 
-    driver.quit()
+        driver.quit()
 
-    return {
-        "temperature": (to_float(temperature) - 32) * 5 / 9,
-        "dew_point": (to_float(dew_point) - 32) * 5 / 9,
-        "humidity": to_float(humidity),
-        "wind": wind,
-        "wind_speed": to_float(wind_speed),
-        "wind_gust": to_float(wind_gust),
-        "pressure": to_float(pressure) * 1e-5,
-        "perspitation": to_float(precipitation),
-        "condition": condition,
-        "visibility": visibility,
-    }
-
+        return {
+            "temperature": (to_float(temperature) - 32) * 5 / 9,
+            "dew_point": (to_float(dew_point) - 32) * 5 / 9,
+            "humidity": to_float(humidity),
+            "wind": wind,
+            "wind_speed": to_float(wind_speed),
+            "wind_gust": to_float(wind_gust),
+            "pressure": to_float(pressure) * 1e-5,
+            "perspitation": to_float(precipitation),
+            "condition": condition,
+            "visibility": visibility,
+        }
+    except:
+        driver.quit()
+        return {
+            "temperature": 'inconnu',
+            "dew_point": 'inconnu',
+            "humidity": 'inconnu',
+            "wind": 'inconnu',
+            "wind_speed": 'inconnu',
+            "wind_gust": 'inconnu',
+            "pressure": 'inconnu',
+            "perspitation": 'inconnu',
+            "condition": 'inconnu',
+            "visibility": 'inconnu',
+        }
 def getCityNameFromCoordinates(latitude, longitude):
     url = f'http://api.3geonames.org/{latitude},{longitude}.json'
     response = requests.get(url)
@@ -121,7 +137,7 @@ def writeToCSV(scrappedData, time, id, filename):
 
 def run(latitude: str, longitude: str, timeofDay:str, id:str, filename: str):
     cityName = getCityNameFromCoordinatesBDC(latitude=latitude, longitude=longitude)
-    scrappedData = getScrappedData(cityName, datetime.datetime.fromisoformat(timeofDay))
-    writeToCSV(scrappedData, datetime.datetime.fromisoformat(timeofDay), id, filename)
-    return 0
+    # scrappedData = getScrappedData(cityName, datetime.datetime.fromisoformat(timeofDay))
+    # writeToCSV(scrappedData, datetime.datetime.fromisoformat(timeofDay), id, filename)
+    return getScrappedData(cityName, datetime.datetime.fromisoformat(timeofDay))
     
